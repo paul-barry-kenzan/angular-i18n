3bfea75d24b6fc982ba1175f3536e533ac177e07
Paul Barry <pbarry@kenzan.com>
2016-01-15 15:42:13 -0500

    Add libraries to app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name InternationalizationModule
+ * @description
+ * # InternationalizationModule
+ *
+ * Main module of the application.
+ */
+angular
+  .module('InternationalizationModule', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'pascalprecht.translate',// angular-translate
+    'tmh.dynamicLocale'// angular-dynamic-locale
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
